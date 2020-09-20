187659fa88c01967b9833a891540ee60407dd0d1
ali-djemmal <menaouerdj@gmail.com>
2020-09-20 14:32:41 +0100

    commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,31 +1,11 @@
-import Vue from "vue";
 import "core-js/es/object/assign";
-import { populateAmenitiesAndPrices } from "./helpers";
-import ModalWindow from "../components/ModalWindow.vue";
-import ImageCarousel from "../components/ImageCarousel.vue";
-import HeaderImage from "../components/HeaderImage.vue";
 
-let model = JSON.parse(window.vuebnb_listing_model);
-model = populateAmenitiesAndPrices(model);
-//-------------------------------------------------------------
+import Vue from 'vue';
+import router from './router'
 
-//---------------------------------------------
+import ListingPage from '../components/ListingPage.vue';
 var app = new Vue({
-    el: "#app",
-    data: Object.assign(model, {
-        headerImageStyle: {
-            "background-image": `url(${model.images[0]})`,
-        },
-        contracted: true,
-    }),
-    components: {
-        ImageCarousel,
-        ModalWindow,
-        HeaderImage,
-    },
-    methods: {
-        openModal() {
-            this.$refs.imagemodal.modalOpen = true;
-        },
-    },
+el: '#app',
+render: h => h(ListingPage),
+router
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,9 +16,11 @@
     <link rel="stylesheet" href="style.css" type="text/css" /> --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
 <link rel="stylesheet" href="{{ asset('css/vue-style.css') }}" type="text/css">
+
     <script type="text/javascript">
        window.vuebnb_listing_model = "{!! addslashes(json_encode($model)) !!}"
       </script>
+      
   </head>
 
   <body>
@@ -28,61 +30,10 @@
       </div>
 
     <div id="app">
-    <header-image :image-url="images[0]" @header-clicked="openModal"></header-image>
-      <div class="container">
-        <div class="heading">
-          <h1>@{{ title }}</h1>
-          <p>@{{ address }}</p>
-        </div>
-        <hr />
-        <div class="about">
-          <h3>About this listing</h3>
-          <p v-bind:class="{ contracted: contracted }">@{{ about }}</p>
-          <button
-            v-if="contracted"
-            class="more"
-            v-on:click="contracted = false"
-          >
-            + More
-          </button>
-          <button
-            v-if="!contracted"
-            class="more"
-            v-on:click="contracted = true"
-          >
-            - Less
-          </button>
-        </div>
-        <div class="lists">
-          <hr />
-          <div class="amenities list">
-            <div class="title"><strong>Amenities</strong></div>
-            <div class="content">
-              <div class="list-item" v-for="amenity in amenities">
-                <i class="fa fa-lg" v-bind:class="amenity.icon"></i>
-                <span>@{{ amenity.title }}</span>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div class="prices list">
-            <div class="title">
-              <strong>Prices</strong>
-            </div>
-            <div class="content">
-              <div class="list-item" v-for="price in prices">
-                @{{ price.title }}: <strong>@{{ price.value }}</strong>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <modal-window ref="imagemodal">
-<image-carousel :images="images"></image-carousel>
-</modal-window>
-
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    
   </body>
+  
+  <script src="{{ asset('js/app.js') }}"></script> 
 </html>
